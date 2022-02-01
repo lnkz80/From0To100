ae09f368f641481aab723bb4eb136f3ec55a565f
LNKZ <lnkz@i.ua>
2022-02-01 14:34:19 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// const arr = [1, 1, 3, 5, 9, 11, 17, 23];
+// console.log(null);
+
+let a = 7;
+
+const car = {
+  color: "red",
+  maxSpeed: 260,
+  getMaxSpeed() {
+    console.log(this.maxSpeed);
+  },
+};
+
+let { x, y } = [0, 1];
+
+console.log(x, y);
+
+// console.log(car);
+
+// car.getMaxSpeed();
+
+// let paragrs = document.querySelectorAll("p");
+// console.log(paragrs);
