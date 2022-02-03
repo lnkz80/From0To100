90583f700bab61cad49922a66df6ecf606d1c9a8
LNKZ <lnkz@i.ua>
2022-02-03 14:50:27 +0200

    create personalMovieDB and ask 3 questions from user

--- script/main.js
+++ script/main.js
@@ -1,1 +1,22 @@
 "use strict";
+
+//1. "Сколько фильмов Вы уже посмотрели?"
+
+let numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?");
+
+//2. Create object "personalMovieDB"
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+//3.Два вопроса
+
+let lastFilm = prompt("Один из последних просмотренных фильмов?");
+personalMovieDB.movies[`'${lastFilm}'`] = prompt("На сколько оцените его?");
+
+console.log(personalMovieDB);
