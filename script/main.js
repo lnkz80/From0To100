e6cfd5795b593aceef6b26f94128d6857ed71a0b
lnkz <lnkz80@gmail.com>
2022-02-10 20:57:00 +0200

    practice_ifelse_for_while_2nd

--- script/main.js
+++ script/main.js
@@ -1,13 +1,6 @@
 "use strict";
 
-//1. "Сколько фильмов Вы уже посмотрели?"
-
-let numberOfFilms = +prompt(
-  "Сколько фильмов Вы уже посмотрели?",
-  "Введите число"
-);
-
-//2. Create object "personalMovieDB"
+let numberOfFilms;
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -17,9 +10,33 @@ const personalMovieDB = {
   privat: false,
 };
 
-//3.Два вопроса
+const userQuest = ["Сколько фильмов Вы уже посмотрели?", 
+"Один из последних просмотренных фильмов?", 
+"На сколько оцените его?"];
 
-let lastFilm = prompt("Один из последних просмотренных фильмов?");
-personalMovieDB.movies[`'${lastFilm}'`] = prompt("На сколько оцените его?");
+// const userQuestDo = []
+let lastFilm = "";
+
+for(let i = 0; i < userQuest.length; i++){
+  if (i === 0 ) {
+    personalMovieDB.count = +prompt(
+      userQuest[i],
+      "Введите число");
+    console.log(personalMovieDB.count);
+  } else if(i === 1){
+      lastFilm = prompt(userQuest[i]);
+    } else if ( i === 2 ){
+      personalMovieDB.movies[`'${lastFilm}'`] = prompt(userQuest[i]);
+    } else {break;} 
+  
+}
+
+// numberOfFilms = +prompt(
+//   "Сколько фильмов Вы уже посмотрели?",
+//   "Введите число"
+// );
+
+// let lastFilm = prompt("Один из последних просмотренных фильмов?");
+// personalMovieDB.movies[`'${lastFilm}'`] = prompt("На сколько оцените его?");
 
 console.log(personalMovieDB);
--- script/test.js
+++ script/test.js
@@ -1,6 +1,16 @@
-let i = 1;
+// let i = 1;
 
-while (i < 5) {
-  console.log(i);
-  i++;
-}
+// while (i < 5) {
+//   console.log(i);
+//   i++;
+// }
+
+
+// for (let i = 0; i < 100; i = i + 10){
+
+//   if (i===50){
+//     continue;
+//   }
+//   console.log(i);
+// }
+
