c5d18949646b9db9407ef56deb654c4f964f7b12
lnkz80 <lnkz80@gmail.com>
2022-02-10 22:19:05 +0200

    сделать проверку на ввод в формах по заданию 2 пункт 2 уроки про условия и циклы

--- script/main.js
+++ script/main.js
@@ -1,35 +1,53 @@
 "use strict";
 
-let numberOfFilms;
-
 const personalMovieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
   privat: false,
 };
 
-const userQuest = ["Сколько фильмов Вы уже посмотрели?", 
-"Один из последних просмотренных фильмов?", 
-"На сколько оцените его?"];
+
+// 1. Сколько фильмов Вы уже просмотрели? + 3. Проверка на киномана
+
+personalMovieDB.count = +prompt(
+  "Сколько фильмов Вы уже посмотрели?",
+  "Введите число");
+
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено довольно мало фильмов...");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert ("Вы киноман однако :)");
+} else {
+  alert("Произошла ошибка :(");
+}
+
+// 1.2. Автоматизация вопросов
+
+let cyclesNum = +prompt("Сколько фильмов Вы хотите оценить?");
+
+for (let i = 1; i <= cyclesNum; i++) {
+  let lastFilm = prompt("Один из последних просмотренных фильмов?");
+  personalMovieDB.movies[`'${lastFilm}'`] = prompt("На сколько оцените его?");
+}
+
+
 
 // const userQuestDo = []
-let lastFilm = "";
+// let lastFilm = "";
 
-for(let i = 0; i < userQuest.length; i++){
-  if (i === 0 ) {
-    personalMovieDB.count = +prompt(
-      userQuest[i],
-      "Введите число");
-    console.log(personalMovieDB.count);
-  } else if(i === 1){
-      lastFilm = prompt(userQuest[i]);
-    } else if ( i === 2 ){
-      personalMovieDB.movies[`'${lastFilm}'`] = prompt(userQuest[i]);
-    } else {break;} 
+// for(let i = 0; i < userQuest.length; i++){
+//   if (i === 0 ) {
+   
+//     console.log(personalMovieDB.count);
+//   } else if(i === 1){
+//       lastFilm = prompt(userQuest[i]);
+//     } else if ( i === 2 ){
+//       personalMovieDB.movies[`'${lastFilm}'`] = prompt(userQuest[i]);
+//     } else {break;} 
   
-}
+// }
 
 // numberOfFilms = +prompt(
 //   "Сколько фильмов Вы уже посмотрели?",
