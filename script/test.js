f2b1ca82471e3d81397fbc6b4adbde18122be404
lnkz <lnkz80@gmail.com>
2022-02-16 09:51:08 +0200

    testFiles

--- script/test.js
+++ script/test.js
@@ -5,3 +5,33 @@
 
 // console.log(wrd.slice(9, wrdLngt - 2));
 
+const testP = document.querySelector("#testP");
+testP.innerHTML = "HOW DO YOU DO?";
+
+// const options = {
+//     width: 1980,
+//     height: 1080,
+//     dpi: 300,
+//     colors: {
+//         bg: 'green',
+//         border: 'white'
+//     }
+// };
+
+// // let {...} = options;
+// console.log(options);
+
+const arr = [1, 4, 34, 65, 32, 21, 56];
+
+
+
+// arr.forEach((item, index)=>{
+//     console.log(`${index} : ${item}`);
+// });
+
+let str = "some";
+let uppStr = str.toLocaleUpperCase();
+
+console.log(typeof(str));
+console.log(typeof(str.toLocaleUpperCase()));
+
